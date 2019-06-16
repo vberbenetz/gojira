c9f70bcf31e1064d5d0731bdeed9bc56015833b7
Val Berbenetz <val.berbenetz@slalom.com>
2019-06-16 19:35:53 -0400

    Added get application role by key url param

--- gojira/applicationRole.go
+++ gojira/applicationRole.go
@@ -2,14 +2,15 @@ package gojira
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 )
 
+// ApplicationRoleService is used to execute requests pertaining to ApplicationRoles
 type ApplicationRoleService service
 
-/**
- Official Docs: https://developer.atlassian.com/cloud/jira/platform/rest/v3/#api-rest-api-3-applicationrole-get
- */
+// ApplicationRole is the response body as outlined by the Jira Cloud API
+// Official Docs: https://developer.atlassian.com/cloud/jira/platform/rest/v3/#api-group-Application-roles
 type ApplicationRole struct {
 	Key string `json:"key"`
 	Groups []string `json:"groups"`
@@ -25,6 +26,7 @@ type ApplicationRole struct {
 	Platform bool `json:"platform"`
 }
 
+// List returns a list of ApplicationRoles via the GET endpoint
 func (s *ApplicationRoleService) List (ctx context.Context) ([]*ApplicationRole, *http.Response, error) {
 	req, err := s.client.NewRequest("GET", "applicationrole", nil)
 	if err != nil {
@@ -38,4 +40,21 @@ func (s *ApplicationRoleService) List (ctx context.Context) ([]*ApplicationRole,
 	}
 
 	return ar, resp, nil
+}
+
+// Get returns a single ApplicationRole by the provided key parameter
+func (s *ApplicationRoleService) Get (ctx context.Context, key string) (*ApplicationRole, *http.Response, error) {
+	endpoint := fmt.Sprintf("applicationrole/%v", key)
+	req, err := s.client.NewRequest("GET", endpoint, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	var ar *ApplicationRole
+	resp, err := s.client.Do(ctx, req, &ar)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return ar, resp, err
 }
